528fd799b0f7550944a45210f99e23c10ba729db
li_mic000 <li_mic000@CAM-LAB-09.bgsd.k12.wa.us>
2019-10-08 07:50:23 -0700

    10/8/2019 Morning

--- src/Hourglass.java
+++ src/Hourglass.java
@@ -1,7 +1,39 @@
 /*
+draw dashed line
+	make a function:
+		draw | sign
+		draw 10 " s
+		draw another | sign
+draw top of hour glass
+	for (lines 1 - 4)
+		draw backslash
+		draw (10 - 2*line) dots (:) on the output line
+		draw slash
+		go to new line
+draw bottom of hourglass
+	for (lines 1- 4)
+		draw slash
+		draw (2 + 2*line) dots (:) on the output line
+		draw backslash
+		go to new line
+draw dashed line again
 
 */
 
 public class Hourglass {
+	public static final int HALF_HEIGHT = 4;
+	
+	public static void main(String[] args) {
+		dashedLine();
+	}
+	
+	public static void dashedLine() {
+		System.out.println("|");
+		for (int i = 1; i <= (2+2*HALF_HEIGHT); i++) {
+			System.out.println("\"");
+		}
+		System.out.println("|");
+	}
+
 
 }
